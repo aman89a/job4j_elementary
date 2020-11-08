600e5a2c4bee3f6ab180a6d688445206824d774a
aman <abdulaev25@gmail.com>
2020-11-08 12:12:07 +0600

    5.7. Ипотека [#378874] исправил ошибку

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        long summ = amount + ((amount * Math.round(percent)) / 100);
-        while(summ >= 0){
-            summ = summ - salary;
+        while (amount > 0) {
+            amount = (int) (amount + ((amount * Math.round(percent)) / 100));
+            amount -= salary;
             year++;
         }
         return year;
