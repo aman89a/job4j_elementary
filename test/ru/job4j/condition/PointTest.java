77e91c154e6853394090931b8bfc98ec16b984b4
aman <abdulaev25@gmail.com>
2020-12-05 08:18:16 +0600

    4. Расстояние между точками в трехмерном пространстве. [#378973]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,54 +3,23 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PointTest {
 
     @Test
-    public void when00to20Then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double expected = 2.00;
-        double out = Point.distance(x1, y1, x2, y2);
+    public void when01to02Then2() {
+        Point a = new Point(0, 0, 1);
+        Point b = new Point(0, 2, 3);
+        double expected = 2.0;
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when00to30Then3() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 3;
-        int y2 = 0;
-        double expected = 3.00;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
-    }
-
-    @Test
-    public void when00to22Then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 2;
+    public void when001to023Then282() {
+        Point a = new Point(0, 0, 1);
+        Point b = new Point(0, 2, 3);
         double expected = 2.82;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
-
-    }
-
-    @Test
-    public void when10to20Then1() {
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double expected = 1.00;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
     }
 }
